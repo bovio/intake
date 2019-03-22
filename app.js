c2afc3d0a76a6f92cf97596513f716915a1edf14
bovio <christopher.bovio@gmail.com>
2019-03-22 08:34:49 -0400

    outputs to text field

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ class Intake {
     bin,
     user
   ) {
-    this.form = document.querySelector("form");
-    this.form.onsubmit = this.onSubmit;
     this.problem = problem;
     this.visual = visual;
     this.accidental = accidental;
@@ -24,22 +22,103 @@ class Intake {
     this.bin = bin;
     this.user = user;
   }
-  onSubmit(e) {
-    e.preventDefault();
-    console.log(e.target);
+  renderCustomerNotes() {
+    if (this.problem !== "") {
+      let external = document.createElement("textarea");
+      let externalHead = document.createElement("h2");
+      externalHead.className = "external-head";
+      externalHead.innerText = "external Notes:";
+      external.setAttribute("class", "customer-facing");
+      external.setAttribute("autofocus", "true");
+      external.innerHTML = `
+      Problem description: ${this.problem} 
+      Visual notes: ${this.visual}
+      Accidental damage: ${this.accidental}
+      Customer expectations: ${this.expectations}
+      Needs data? ${this.data}
+      Password: ${this.password}
+      Accessories: ${this.accessories}
+      Internal notes: ${this.internal}
+      BIN: ${this.bin}
+      User: ${this.user}
+      `;
 
-    let values = Array.from(e.target);
-    const valuesArray = () => {
-      return values.forEach((x, i) => {});
-    };
-    console.log(valuesArray());
+      if (document.getElementById("external-notes").children.length <= 1) {
+        document
+          .getElementById("external-notes")
+          .firstElementChild.appendChild(external);
+
+        document
+          .getElementById("external-notes")
+          .insertBefore(
+            externalHead,
+            document.getElementById("external-notes").firstElementChild
+          );
+      } else {
+        document
+          .getElementById("external-notes")
+          .children[1].replaceChild(
+            external,
+            document.getElementById("external-notes").children[1].childNodes[0]
+          );
+      }
+    }
   }
 }
 
-new Intake();
+class UI {}
 
-document.getElementById("damage").addEventListener("change", function(e) {
-  console.log(e.target.nextElementSibling.nextElementSibling.children);
+document.querySelector("form").addEventListener("submit", function(e) {
+  document.querySelector(".btn-submit").innerText = "update";
+  let intake = new Intake();
+  intake.problem = document.getElementById("problem").value;
+  intake.visual = document.getElementById("visual").value;
+  intake.expectations = document.getElementById("expectations").value;
+  intake.password = document.getElementById("password").children[1].value;
+
+  intake.internal = document.getElementById("internal").value;
+  intake.bin = document.getElementById("bin").value;
+  intake.user = document.getElementById("user").value;
+
+  if (
+    document
+      .getElementById("accidental")
+      .children[5].querySelector("textarea") != null
+  ) {
+    intake.accidental = document.getElementById(
+      "accidental"
+    ).children[5].children[0].value;
+  } else {
+    intake.accidental = "no damage";
+  }
+
+  if (
+    document
+      .getElementById("accessories")
+      .children[5].querySelector("textarea") != null
+  ) {
+    intake.accessories = document.getElementById(
+      "accessories"
+    ).children[5].children[0].value;
+  } else {
+    intake.accessories = "no accessories";
+  }
+
+  document
+    .getElementById("data")
+    .querySelectorAll("input")
+    .forEach(option => {
+      if (option.checked) {
+        intake.data = option.value;
+      }
+    });
+
+  intake.renderCustomerNotes();
+
+  e.preventDefault();
+});
+
+document.getElementById("accidental").addEventListener("change", function(e) {
   if (
     e.target.value === "yes" &&
     e.target.nextElementSibling.nextElementSibling.children.length === 0
@@ -52,9 +131,11 @@ document.getElementById("damage").addEventListener("change", function(e) {
     textArea.className = "damage external";
     damageDiv.append(textArea);
   }
+  if (e.target.value === "no") {
+    document.querySelector(".accident-box").firstElementChild.remove();
+  }
 });
 document.getElementById("accessories").addEventListener("change", function(e) {
-  console.log(e.target.nextElementSibling.nextElementSibling.children);
   if (
     e.target.value === "yes" &&
     e.target.nextElementSibling.nextElementSibling.children.length === 0
@@ -70,4 +151,12 @@ document.getElementById("accessories").addEventListener("change", function(e) {
     textArea.className = "damage external";
     damageDiv.append(textArea);
   }
+
+  if (e.target.value === "no") {
+    document.querySelector(".accessories-box").firstElementChild.remove();
+  }
+});
+
+document.querySelector(".btn-clear").addEventListener("click", function() {
+  window.location.reload();
 });
